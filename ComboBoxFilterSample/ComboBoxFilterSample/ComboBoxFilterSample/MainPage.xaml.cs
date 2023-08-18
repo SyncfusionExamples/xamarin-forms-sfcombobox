dfa3dc1569c4c67f7a5d9080057030df7f971a5c
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-08-18 10:22:38 +0530

    Added example sample for SfComboBox Filtering feature

---
+++ ComboBoxFilterSample/ComboBoxFilterSample/ComboBoxFilterSample/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Syncfusion.XForms.ComboBox;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ComboBoxFilterSample
+{
+    public partial class MainPage : ContentPage
+    {
+        ListView MainListView;
+        public MainPage()
+        {
+            InitializeComponent();
+            EmployeeViewModel viewModel = new EmployeeViewModel();
+            this.BindingContext = viewModel;
+            StackLayout stackLayout = new StackLayout()
+            {
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Start,
+                Padding = new Thickness(30),
+                Margin = new Thickness(50)
+            };
+
+            SfComboBox comboBox = new SfComboBox()
+            {
+                HeightRequest = 40,
+                WidthRequest = 200,
+                IsEditableMode = true,
+                AllowFiltering = true,
+                DisplayMemberPath = "Name",
+                SuggestionBoxPlacement = SuggestionBoxPlacement.None,
+                SuggestionMode = SuggestionMode.Contains,
+                DataSource = viewModel.EmployeeCollection
+            };
+
+            comboBox.FilterCollectionChanged += ComboBox_FilterCollectionChanged;
+            Binding filteredItemsBinding = new Binding();
+            filteredItemsBinding.Source = viewModel;
+            filteredItemsBinding.Path = "Items";
+            filteredItemsBinding.Mode = BindingMode.TwoWay;
+            comboBox.SetBinding(SfComboBox.FilteredItemsProperty, filteredItemsBinding);
+            StackLayout stack = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical,
+                Margin = new Thickness(0, 50, 0, 0)
+            };
+
+            Label filteredItemsLabel = new Label()
+            {
+                Text = "Filtered Items",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.Green
+            };
+
+            MainListView = new ListView()
+            {
+                RowHeight = 30
+            };
+
+            DataTemplate itemTemplate = new DataTemplate(() =>
+            {
+                StackLayout layout = new StackLayout()
+                {
+                    Orientation = StackOrientation.Horizontal
+                };
+
+                Label nameLabel = new Label();
+                nameLabel.SetBinding(Label.TextProperty, "Name");
+                layout.Children.Add(nameLabel);
+
+                return new ViewCell { View = layout };
+            });
+
+            MainListView.ItemTemplate = itemTemplate;
+
+            stack.Children.Add(filteredItemsLabel);
+            stack.Children.Add(MainListView);
+            stackLayout.Children.Add(comboBox);
+            stackLayout.Children.Add(stack);
+            this.Content = stackLayout;
+        }
+        private void ComboBox_FilterCollectionChanged(object sender, FilterCollectionChangedEventArgs e)
+        {
+            MainListView.ItemsSource = (IEnumerable)e.Value;
+        }
+    }
+}
---
+++ ComboBoxFilterSample/ComboBoxFilterSample/ComboBoxFilterSample/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ComboBoxFilterSample
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private IEnumerable<object> items;
+
+        public IEnumerable<object> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ObservableCollection<Employee> employeeCollection;
+        public ObservableCollection<Employee> EmployeeCollection
+        {
+            get { return employeeCollection; }
+            set { employeeCollection = value; }
+        }
+        public EmployeeViewModel()
+        {
+            employeeCollection = new ObservableCollection<Employee>();
+            employeeCollection.Add(new Employee() { ID = 1, Name = "Eric" });
+            employeeCollection.Add(new Employee() { ID = 2, Name = "James" });
+            employeeCollection.Add(new Employee() { ID = 3, Name = "Jacob" });
+            employeeCollection.Add(new Employee() { ID = 4, Name = "Lucas" });
+            employeeCollection.Add(new Employee() { ID = 5, Name = "Mark" });
+            employeeCollection.Add(new Employee() { ID = 6, Name = "Aldan" });
+            employeeCollection.Add(new Employee() { ID = 7, Name = "Aldrin" });
+            employeeCollection.Add(new Employee() { ID = 8, Name = "Alan" });
+            employeeCollection.Add(new Employee() { ID = 9, Name = "Aaron" });
+        }
+    }
+}
