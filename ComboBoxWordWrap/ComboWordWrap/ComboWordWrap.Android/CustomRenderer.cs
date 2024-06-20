a8ea9859cd2bbeeb1bb842db4ea4c9424f79d041
Brundha <brundha.sf3969@gmail.com>
2024-06-20 16:58:09 +0530

    Added sample for ComboBoxWordWrap

---
+++ ComboBoxWordWrap/ComboWordWrap/ComboWordWrap.Android/CustomRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ComboWordWrap;
+using ComboWordWrap.Droid;
+using Syncfusion.Android.ComboBox;
+using Syncfusion.XForms.Android.ComboBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+[assembly: ExportRenderer(typeof(SfCustomComboBox), typeof(CustomRenderer))]
+namespace ComboWordWrap.Droid
+{
+    public class CustomRenderer:SfComboBoxRenderer
+    {
+        public CustomRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Syncfusion.XForms.ComboBox.SfComboBox> e)
+        {
+            base.OnElementChanged(e);
+            Control.GetAutoEditText().SetLines(8);
+            Control.GetAutoEditText().SetHorizontallyScrolling(false);
+        }
+    }
+}
